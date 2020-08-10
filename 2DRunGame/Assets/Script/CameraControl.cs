5f13389d9d5ec026d989fa981f6f7b276cb82d5c
Kirito870425 <50912952+Kirito870425@users.noreply.github.com>
2020-08-10 21:35:16 +0800

    貓神偷完成

---
+++ 2DRunGame/Assets/Script/CameraControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Transform target;
+    [Range(0,100)]
+    public float speed = 1;
+    [Header("攝影機拍攝的上下限")]
+    public Vector2 limit = new Vector2(0, 0.7f);
+
+
+    /// <summary>攝影機追蹤</summary>
+    private void Track()
+    {
+        Vector3 posA = transform.position;  //攝影機
+        Vector3 posB = target.position;     //目標:貓
+
+        posB.z = -10;
+        posB.y = Mathf.Clamp(posB.y, limit.x, limit.y);     //夾住
+
+        posA = Vector3.Lerp(posA, posB, speed * Time.deltaTime);    //差值Lerp:趨近於兩點
+
+        transform.position = posA;
+    }
+    //攝影機官方建議放這:延遲Updata
+    private void LateUpdate()
+    {
+        Track();
+    }
+}
--- 2DRunGame/Assets/Script/Player.cs
+++ 2DRunGame/Assets/Script/Player.cs
@@ -1,28 +1,34 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     #region 欄位
-    [Header("移動速度"), Range(0, 1000)]
+    [Header("移動速度"), Range(0, 10)]
     public float speed = 5;
     [Header("跳越高度"), Range(0, 1000)]
     public int jump = 320;
     [Header("血量"), Range(0, 2000)]
     public float hp = 500;
-    public bool isGround;
-    public int coin;
+    private float hpmax = 500;
+    public Image imagehp;
+    private bool dead;
+    private bool isGround;
 
     [Header("音效區域")]
     public AudioClip soundHit;
     public AudioClip soundSlide;
     public AudioClip soundJump;
     public AudioClip soundCoin;
-
+    public AudioSource audiojump;
+    [Header("其他區域")]
+    public Text textcoin;
     public Animator catanimator;
     public Rigidbody2D rg;
     public CapsuleCollider2D cap;
-    public AudioSource audiojump;
-    public AudioSource audioslide;
+    public GameObject final;
+    public int coin;
+
 
     #endregion
 
@@ -36,7 +42,7 @@ public class Player : MonoBehaviour
     public void Jump()
     {
         bool playerspace = Input.GetKeyDown(KeyCode.Space);
-        // 2射線碰撞物體 2D物理.射線碰撞(起點, 方向, 長度, 圖層)
+        // 2射線碰撞物體 2D物理.射線碰撞(起點, 方向, 長度, 圖層(比Tag多"物理"判定))
         RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3(0, -2.1f), -transform.up, 0.1f, 1 << 8);
 
         if (hit)
@@ -60,15 +66,15 @@ public class Player : MonoBehaviour
 
         }
     }
-    
+
     /// <summary>滑行</summary>
     public void Slider()
     {
         bool playerslider = Input.GetKey(KeyCode.LeftControl);
         catanimator.SetBool("CatSllder", playerslider);
 
-        if(Input.GetKeyDown(KeyCode.LeftControl))
-            audioslide.PlayOneShot(soundSlide);
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+            audiojump.PlayOneShot(soundSlide);
 
         //按下左CTRL滑行變更碰撞器大小
         if (playerslider)
@@ -85,32 +91,57 @@ public class Player : MonoBehaviour
         }
     }
     /// <summary>吃金幣</summary>
-    public void EatCoin()
+    public void EatCoin(GameObject objcoin)
     {
-
+        coin++;
+        audiojump.PlayOneShot(soundCoin);
+        textcoin.text = "金幣：" + coin;
+        Destroy(objcoin, 0);    //Object不用開頭. ,延遲
     }
     /// <summary>受傷</summary>
-    public void Hit()
+    public void Hit(GameObject objhit)
     {
+        hp -= 100;
+        audiojump.PlayOneShot(soundHit);
+        imagehp.fillAmount = hp / hpmax;
+        Destroy(objhit);
 
+        if (hp <= 0) Dead();
     }
+
     /// <summary>死亡</summary>
     public void Dead()
     {
-
+        catanimator.SetTrigger("CatDead");
+        final.SetActive(true);
+        speed = 0;
+        dead = true;
+        texttitle.text = "還差的遠呢 嫩";
     }
+    public Text texttitle;
+    public Text textfinalcoin;
+
     /// <summary>過關</summary>
     public void Pass()
     {
-
+        speed = 0;
+        final.SetActive(true);
+        texttitle.text = "恭喜你過關RRRRRR";
+        textfinalcoin.text = "本次金幣數量" + coin;
     }
     #endregion
 
     #region 事件
-    
+    private void Start()
+    {
+        hpmax = hp;
+    }
 
     private void Update()
     {
+        if (dead) return;   //return以下不會執行
+        if (transform.position.y <= -6f) Dead();
+
         Move();
         Jump();
         Slider();
@@ -126,6 +157,19 @@ public class Player : MonoBehaviour
         //DrawLine 點對點
         Gizmos.DrawRay(transform.position + new Vector3(0, -2.1f), -transform.up * 0.5f);
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "金幣")
+            EatCoin(collision.gameObject);
+        if (collision.tag == "陷阱")
+            Hit(collision.gameObject);
+        if (collision.name == "傳送門")
+            Pass();
+    }
+    //enter 進入執行一次
+    //exit 離開
+    //stay 持續執行 60FPS
     #endregion
 
 }
